ef288dcdd2c7459ff4fc4ebb9c1f025fa0811bff
ErnestTester <profile.tester1@outlook.com>
2024-09-09 14:02:27 -0600

    Create a project

---
+++ Project/Project/MyLog.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    public class MyLog
+    {
+        private readonly string logFilePath;
+        
+
+        public MyLog(string filePath)
+        {
+            logFilePath=filePath;
+        }
+        public void Log( string level, string message)
+        {
+            try
+            {
+                string logMessage = $"{DateTime.Now:yyyy-MM-dd HH:mm:ss} [{level}]-{message}";
+                File.AppendAllText(logFilePath, logMessage + Environment.NewLine);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error logging message: {ex.Message}");
+            }
+        }
+
+            public void Info(string message)
+            {
+                Log("INFO", message);
+            }
+
+            public void Warning(string message)
+            {
+                Log("WARNING", message);
+            }
+
+            public void Error(string message)
+            {
+                Log("ERROR", message);
+            }
+        }
+
+    
+
+}
---
+++ Project/Project/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.IO;
+
+namespace Project
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Stat!");
+            MyLog logger = new MyLog("LogProject.txt");
+
+         
+      
+            logger.Info("User logged in.");
+            logger.Warning("Failed login attempt.");
+            Console.WriteLine("End to capture the logs");
+        }
+            
+           
+    }
+}
