16ffcb08ecac20ce9e25fe82c737de3b68c3f23d
ErnestTester <profile.tester1@outlook.com>
2024-09-09 18:17:17 -0600

    Add a project for sort a list of products name, price, or stock

---
+++ InventoryMgment/InventoryMgment/Product.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryMgment
+{
+    public  class Product
+    {
+        public string Name 
+        { 
+            get;
+            set;
+        }
+        public decimal Price
+        { 
+            get; 
+            set;
+        }
+        public int Stock
+        { 
+            get; 
+            set;
+        }
+    };
+
+    public class InventoryManager
+    {
+        public List<Product> SortProducts(List<Product> products, string sortKey, bool ascending)
+        {
+            IEnumerable<Product> sortedProducts = null;
+
+            switch (sortKey.ToLower())
+            {
+                case "name":
+                    sortedProducts = ascending ? products.OrderBy(p => p.Name) : products.OrderByDescending(p => p.Name);
+                    break;
+                case "price":
+                    sortedProducts = ascending ? products.OrderBy(p => p.Price) : products.OrderByDescending(p => p.Price);
+                    break;
+                case "stock":
+                    sortedProducts = ascending ? products.OrderBy(p => p.Stock) : products.OrderByDescending(p => p.Stock);
+                    break;
+                default:
+                    throw new ArgumentException("Invalid sort key.");
+            }
+
+            return sortedProducts.ToList();
+        }
+    }
+}
---
+++ InventoryMgment/InventoryMgment/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryMgment
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var products = new List<Product>
+            {
+            new Product { Name = "Product A", Price = 100, Stock = 5 },
+            new Product { Name = "Product B", Price = 200, Stock = 3 },
+            new Product { Name = "Product C", Price = 50, Stock = 10 },
+            new Product { Name = "Product D", Price = 1000, Stock = 2 }
+            };
+
+
+            InventoryManager inventoryManager = new InventoryManager();
+
+            var sortedProducts = inventoryManager.SortProducts(products, "price", false);
+
+            foreach (var product in sortedProducts)
+            {
+                Console.WriteLine($"Name: {product.Name}, Price: {product.Price}, Stock: {product.Stock}");
+            }
+            foreach (var product in sortedProducts)
+            {
+                Console.WriteLine( $"Price: { product.Price}, Name: {product.Name}");
+            }
+            foreach (var product in sortedProducts)
+            {
+                Console.WriteLine($"Stock: {product.Stock}, Name: {product.Name}");
+            }
+        }
+    }
+}
