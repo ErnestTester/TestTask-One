66a86eadb157cce5394e6fde2c3cce006e8e64de
ErnestTester <profile.tester1@outlook.com>
2024-09-18 18:31:35 -0600

    Add a new list to declare names in a list and fint a specifi value

---
+++ InventoryMgment/WorkingList/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace WorkingList   
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<string> names = ["Ernesto", "Ana", "Felipe","Alice", "Bob", "Charlie", "David", "Eva",
+            "Frank", "Grace", "Hannah", "Ian", "Jack", "Karen", "Liam", "Mona", "Nina", "Oscar"  ];
+            foreach (string name in names)
+            {
+                Console.WriteLine($"Hello {name.ToUpper()}!");
+
+
+            }
+
+            var index = names.IndexOf("Liam");
+            if(index==-1)
+            {
+                Console.WriteLine($"When an item is not found, IndexOf returns {index}");
+            }
+            else
+            {
+                Console.WriteLine($"The name {names[index]} is at the index {index}");
+                Console.WriteLine($"We find you! {names[index]}!");
+            }
+
+        }
+    }
+}
